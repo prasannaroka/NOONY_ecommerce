636831a1f0e63ea8367eea57bc66c3e53b8c2eca
prasanna roka <prasannaroka600@gmail.com>
2023-08-09 23:48:28 +0545

    Initial commit

---
+++ src/assets/images/index.js
@@ -0,0 +1,56 @@
+import logo from "./orebiLogo.png";
+import logoLight from "./logoLight.png";
+import bannerImgOne from "./banner/bannerImgOne.webp";
+import bannerImgTwo from "./banner/bannerImgTwo.webp";
+import bannerImgThree from "./banner/bannerImgThree.webp";
+import saleImgOne from "./sale/saleImgOne.webp";
+import saleImgTwo from "./sale/saleImgTwo.webp";
+import saleImgThree from "./sale/saleImgThree.webp";
+
+import newArrOne from "./products/newArrival/newArrOne.webp";
+import newArrTwo from "./products/newArrival/newArrTwo.webp";
+import newArrThree from "./products/newArrival/newArrThree.webp";
+import newArrFour from "./products/newArrival/newArrFour.webp";
+
+import bestSellerOne from "./products/bestSeller/bestSellerOne.webp";
+import bestSellerTwo from "./products/bestSeller/bestSellerTwo.webp";
+import bestSellerThree from "./products/bestSeller/bestSellerThree.webp";
+import bestSellerFour from "./products/bestSeller/bestSellerFour.webp";
+
+import spfOne from "./products/specialOffer/spfOne.webp";
+import spfTwo from "./products/specialOffer/spfTwo.webp";
+import spfThree from "./products/specialOffer/spfThree.webp";
+import spfFour from "./products/specialOffer/spfFour.webp";
+
+import productOfTheYear from "./products/productOfTheYear.webp";
+import paymentCard from "./payment.png";
+import emptyCart from "../images/emptyCart.png";
+
+export {
+  logo,
+  logoLight,
+  bannerImgOne,
+  bannerImgTwo,
+  bannerImgThree,
+  saleImgOne,
+  saleImgTwo,
+  saleImgThree,
+  newArrOne,
+  newArrTwo,
+  newArrThree,
+  newArrFour,
+
+  bestSellerOne,
+  bestSellerTwo,
+  bestSellerThree,
+  bestSellerFour,
+
+  spfOne,
+  spfTwo,
+  spfThree,
+  spfFour,
+
+  productOfTheYear,
+  paymentCard,
+  emptyCart,
+};
---
+++ src/components/pageProps/shopPage/Pagination.js
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import ReactPaginate from "react-paginate";
+import Product from "../../home/Products/Product";
+import { paginationItems } from "../../../constants";
+
+const items = paginationItems;
+function Items({ currentItems }) {
+  return (
+    <>
+      {currentItems &&
+        currentItems.map((item) => (
+          <div key={item._id} className="w-full">
+            <Product
+              _id={item._id}
+              img={item.img}
+              productName={item.productName}
+              price={item.price}
+              color={item.color}
+              badge={item.badge}
+              des={item.des}
+            />
+          </div>
+        ))}
+    </>
+  );
+}
+
+const Pagination = ({ itemsPerPage }) => {
+ const [itemOffset, setItemOffset] = useState(0);
+  const [itemStart, setItemStart] = useState(1);
+
+ const endOffset = itemOffset + itemsPerPage;
+  const currentItems = items.slice(itemOffset, endOffset);
+  const pageCount = Math.ceil(items.length / itemsPerPage);
+
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % items.length;
+    setItemOffset(newOffset);
+   setItemStart(newOffset);
+  };
+
+  return (
+    <div>
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-10 mdl:gap-4 lg:gap-10">
+        <Items currentItems={currentItems} />
+      </div>
+      <div className="flex flex-col mdl:flex-row justify-center mdl:justify-between items-center">
+        <ReactPaginate
+          nextLabel=""
+          onPageChange={handlePageClick}
+          pageRangeDisplayed={3}
+          marginPagesDisplayed={2}
+          pageCount={pageCount}
+          previousLabel=""
+          pageLinkClassName="w-9 h-9 border-[1px] border-lightColor hover:border-gray-500 duration-300 flex justify-center items-center"
+          pageClassName="mr-6"
+          containerClassName="flex text-base font-semibold font-titleFont py-10"
+          activeClassName="bg-black text-white"
+        />
+
+        <p className="text-base font-normal text-lightText">
+          Products from {itemStart === 0 ? 1 : itemStart} to {endOffset} of{" "}
+          {items.length}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Pagination;
